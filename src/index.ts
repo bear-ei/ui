3d2523d1bbae0803b2cf86825e920f5f4a91bc4d
Bearei <bear_ei@outlook.com>
2025-04-22 13:48:12 +0800

    feat: add export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export {createToken} from '@bearei/material-token'
+export {
+	CONTRAST,
+	createToken,
+	DURATION,
+	EASING,
+	PALETTE,
+	PLATFORM,
+	SCHEME,
+	SHAPE,
+	SIZE,
+	TYPOGRAPHY,
+	WINDOW_SIZE
+} from '@bearei/material-token'
 export type {Token} from '@bearei/material-token'
 export * from 'class-validator'
 export * from './components'
