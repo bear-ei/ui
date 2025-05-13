c921843c4ba2a35391cd887e85cddd0c0b46e6e6
Bearei <bear_ei@outlook.com>
2025-05-13 09:30:04 +0800

    refactor: Icon

--- src/components/Icon/Icon-base.component.tsx
+++ src/components/Icon/Icon-base.component.tsx
@@ -2,9 +2,9 @@ import {hexToRGBA} from '@bearei/material-token'
 import {forwardRef, useId, useMemo} from 'react'
 import type {View} from 'react-native'
 import {useTheme} from 'styled-components/native'
+import {iconStyleConfig} from './Icon-style'
 import {ICON_NAME, ICON_STYLE, ICON_TYPE} from './Icon.enum'
 import type {IconBaseProps} from './Icon.interface'
-import {iconStyleConfig} from './icon-style'
 
 export const IconBase = forwardRef<View, IconBaseProps>(
 	(
