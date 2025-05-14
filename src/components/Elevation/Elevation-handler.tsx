2ca49f203b4ae99a38bda2db9ef364c224569c83
Bearei <bear_ei@outlook.com>
2025-05-14 13:42:53 +0800

    refactor: refactor animateElevationEffect

--- src/components/Elevation/Elevation-handler.tsx
+++ src/components/Elevation/Elevation-handler.tsx
@@ -1,6 +1,6 @@
 import type {SharedValue} from 'react-native-reanimated'
 import type {AnimatedTiming} from '../../hooks'
 
-export const handleElevationAnimatedTiming =
+export const animateElevationLevel =
 	(animatedTiming: AnimatedTiming) => (shadowSharedValue: SharedValue<number>) => (level: number) =>
 		animatedTiming()(shadowSharedValue)(level)
--- src/components/Elevation/use-elevation-animated.hook.ts
+++ src/components/Elevation/use-elevation-animated.hook.ts
@@ -5,7 +5,7 @@ import {interpolate, useAnimatedStyle, useSharedValue} from 'react-native-reanim
 import type {DefaultStyle} from 'react-native-reanimated/lib/typescript/hook/commonTypes'
 import {useTheme} from 'styled-components/native'
 import {useAnimatedTiming} from '../../hooks'
-import {handleElevationAnimatedTiming} from './Elevation-handle'
+import {animateElevationLevel} from './Elevation-handler'
 import {ELEVATION} from './Elevation.enum'
 import type {UseElevationAnimatedOptions} from './Elevation.interface'
 
@@ -140,14 +140,14 @@ export const useElevationAnimated = ({level = ELEVATION.LEVEL_0}: UseElevationAn
 				}) as DefaultStyle
 	})
 
-	const onElevationAnimatedTiming = useMemo(
-		() => handleElevationAnimatedTiming(animatedTiming)(shadowSharedValue),
+	const animateElevationEffect = useMemo(
+		() => animateElevationLevel(animatedTiming)(shadowSharedValue),
 		[animatedTiming, shadowSharedValue]
 	)
 
 	useEffect(() => {
-		onElevationAnimatedTiming(level)
-	}, [level, onElevationAnimatedTiming])
+		animateElevationEffect(level)
+	}, [level, animateElevationEffect])
 
 	return {shadowAnimatedStyle}
 }
