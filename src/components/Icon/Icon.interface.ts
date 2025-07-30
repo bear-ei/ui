357a36aa42d6f11ded4b34a6537a1247a23824fa
Bearei <bear_ei@outlook.com>
2025-07-30 09:40:58 +0800

    feat: add icon

--- src/components/Icon/Icon.interface.ts
+++ src/components/Icon/Icon.interface.ts
@@ -1,9 +1,10 @@
 import type {FC, RefAttributes} from 'react'
 import type {StyleProp, View, ViewProps, ViewStyle} from 'react-native'
 import type {SvgProps} from 'react-native-svg'
-import type {ICON_NAME, ICON_STYLE, ICON_TYPE} from './Icon.enum'
+import type {ICON_STYLE, ICON_TYPE} from './Icon.enum'
+import type {outlined} from './Icon.outlined'
 
-export type IconName = (typeof ICON_NAME)[keyof typeof ICON_NAME]
+export type IconName = keyof typeof outlined.OUTLINED
 export type IconStyle = (typeof ICON_STYLE)[keyof typeof ICON_STYLE]
 export type IconType = (typeof ICON_TYPE)[keyof typeof ICON_TYPE]
 export interface IconProps extends Omit<SvgProps, 'hitSlop' | 'width' | 'height'>, RefAttributes<View>, ViewProps {
