dbbdf1a85e566bdf4ba3fe23ae1d9312ae989cb7
Bearei <bear_ei@outlook.com>
2025-10-05 13:56:45 +0800

    chore: Delete useless content

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import {Text, View} from 'react-native'
+
+const App: FC = () => (
+        <View>
+                <View>
+                        <Text>Hello World</Text>
+                        <Text>This is the first page of your app.</Text>
+                </View>
+        </View>
+)
+
+export default App
