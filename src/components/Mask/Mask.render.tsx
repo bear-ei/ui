1b9bd4df7e43fc8eb1b177376b45c7041d72f5ab
Bearei <bear_ei@outlook.com>
2025-10-21 09:47:59 +0800

    fix: fix mask

--- src/components/Mask/Mask.render.tsx
+++ src/components/Mask/Mask.render.tsx
@@ -1,6 +1,9 @@
+import {useTheme} from '@/hooks'
+import {hexToRGBA} from '@bearei/theme-token'
 import {clsx} from 'clsx'
 import {forwardRef} from 'react'
-import {Platform, Pressable, View} from 'react-native'
+import {Pressable, View} from 'react-native'
+import {LayoutAnimated} from '../Layout-animated'
 import type {RenderMaskProps} from './Mask.interface'
 
 export const RenderMask = forwardRef<View, RenderMaskProps>(
@@ -10,44 +13,49 @@ export const RenderMask = forwardRef<View, RenderMaskProps>(
                         className,
                         id,
                         interactionHandlers,
-                        opacity,
+                        opacity = 0.2,
                         style,
                         testID,
                         visible,
                         ...containerProps
                 },
                 ref
-        ) => (
-                <View
-                        {...containerProps}
-                        accessibilityRole='alert'
-                        accessible={true}
-                        className={clsx(
-                                'bottom-0 left-0 right-0 top-0 z-40 cursor-default bg-[--color-scrim] opacity-20',
-                                {
-                                        ['-z-40 overflow-hidden']: !visible,
-                                        ['absolute']: Platform.OS !== 'web',
-                                        ['fixed']: Platform.OS === 'web'
-                                },
-                                className
-                        )}
-                        ref={ref}
-                        testID={testID ?? `mask--${id}`}
-                        style={[
-                                style,
-                                {
-                                        ...(typeof opacity === 'number' && {opacity}),
-                                        ...(backgroundColor && {backgroundColor})
-                                }
-                        ]}
-                >
-                        <Pressable
-                                {...interactionHandlers}
-                                className='flex-1 outline-none'
-                                testID={`mask__content--${id}`}
-                        />
-                </View>
-        )
+        ) => {
+                const theme = useTheme()
+
+                return (
+                        <LayoutAnimated
+                                {...containerProps}
+                                visible={visible}
+                                accessibilityRole='alert'
+                                accessible={true}
+                                className={clsx(
+                                        'absolute bottom-0 left-0 right-0 top-0 cursor-default',
+                                        {
+                                                ['overflow-hidden']: !visible,
+                                                ['z-40']: visible
+                                        },
+                                        className
+                                )}
+                                ref={ref}
+                                testID={testID ?? `mask--${id}`}
+                                style={[
+                                        style,
+                                        {
+                                                backgroundColor: hexToRGBA(backgroundColor ?? theme.token.scheme.scrim)(
+                                                        opacity
+                                                )
+                                        }
+                                ]}
+                        >
+                                <Pressable
+                                        {...interactionHandlers}
+                                        className='flex-1 outline-none'
+                                        testID={`mask__content--${id}`}
+                                />
+                        </LayoutAnimated>
+                )
+        }
 )
 
 RenderMask.displayName = 'RenderMask'
